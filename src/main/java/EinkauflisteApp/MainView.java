4fd81b96b652d982f4b893a1ddfb03bb738f114d
FabCo92 <fabiseelert+github@live.de>
2020-07-24 17:14:21 +0200

    ViewClass added

---
+++ src/main/java/EinkauflisteApp/MainView.java
@@ -0,0 +1,32 @@
+package EinkauflisteApp;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception  {
+		 primaryStage.setTitle("Meine Einkaufsliste");
+	        Button btn = new Button();
+	        btn.setText("Neuer Eintrag");
+	        btn.setOnAction(e -> {
+	        	System.out.println("Neuer Eintrag!");
+	        });
+	        
+	        StackPane root = new StackPane();
+	        root.getChildren().add(btn);
+	        primaryStage.setScene(new Scene(root, 300, 250));
+	        primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+}
